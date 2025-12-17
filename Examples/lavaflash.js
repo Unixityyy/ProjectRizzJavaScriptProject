6a69ebfa931e72a45b364b48ac73ae5dbb4b8c95
Shannja <shannuel22m@gmail.com>
2025-12-17 17:40:37 -0600

    made the example use dt instead of Date.now, also replaced == with ===

--- Examples/lavaflash.js
+++ Examples/lavaflash.js
@@ -1,17 +1,15 @@
-let time = Date.now()
+let timer = 0;
 
-function tick(dt)
-{
-    if (Date.now() - time >= 1000)
-    {
-        time = Date.now()
-        if (LocalPlayer.Material == 1)
-        {
-            LocalPlayer.Material = 0
-        }
-        else
-        {
-            LocalPlayer.Material = 1
+function tick(dt) {
+    timer += dt;
+
+    if (timer >= 1.0) {
+        timer = 0;
+        
+        if (LocalPlayer.Material === 1) {
+            LocalPlayer.Material = 0;
+        } else {
+            LocalPlayer.Material = 1;
         }
     }
 }
